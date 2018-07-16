5ec7e1a84cfaba698355f1b1da7b1838786c2ff3
Divyanshu Bhardwaj <kevinclrksn@gmail.com>
2018-07-16 11:38:14 +0530

    Add files via upload

---
+++ ligh.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import time
+root = Tk()
+def cl():
+    l.config(text = "hi")
+l = Label(root,text = "Red",activebackground = 'red')
+l.pack()
+Button(root,text = "change",command = cl).pack()
+root.mainloop()
